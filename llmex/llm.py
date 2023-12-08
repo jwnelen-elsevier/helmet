61ddf21d1d4aad2cbe6c9d417d681094ff3a0f1b
jwnelen-elsevier <jnelen@elsevier.com>
2023-12-08 15:06:07 +0100

    Exploring the tokenization and embeddings

--- llmex/llm.py
+++ llmex/llm.py
@@ -19,6 +19,22 @@ text = "How do I unscrew a screw without a screwdriver?"
 document = """Kitchen butter knives can be used in a very similar way to coins. Insert the end of the butter knife into the longer groove and turn counterclockwise to unscrew the screw.
 If your butter knife is of low quality and strength or the screw is very tight, then you may bend your butter knife rather than unscrewing the screw. Be aware of this potential damage.[1]"""
 
+from scipy.spatial.distance import cosine
+import numpy as np
+
+def cosine_similarity(vector1: torch.Tensor, vector2: torch.Tensor) -> float:
+    tensor1 = torch.tensor(vector1,requires_grad=True)
+    tensor2 = torch.tensor(vector2,requires_grad=True)
+
+    # Ensure that the vectors are numpy arrays
+    vector1 = np.array(tensor1.detach().numpy())
+    vector2 = np.array(tensor2.detach().numpy())
+
+    # Calculate the cosine similarity
+    similarity = 1 - cosine(vector1, vector2)
+
+    return similarity
+
 class LLM(object):
     def __init__(self, name, model_name, tokenizer_name):
         self.name = name
@@ -31,18 +47,28 @@ class LLM(object):
         print("can generate:", self.model.can_generate())
         print("is parallelizable:", self.model.is_parallelizable)
 
-    def tokenize(self, text: str = text) -> torch.Tensor:
-        tokens = self.tokenizer.encode(text, return_tensors='pt')
-        print("tokens: ", tokens)
-        for token in tokens[0]:
-            print("token:", self.tokenizer.decode([token]))
-        return tokens
+    def tokenize(self, text: str):
+        # return self.tokenizer.encode(text, return_tensors='pt')
+        return self.tokenizer.encode(text)
+
+    def display_tokenized_text(self, tokens, index_tokens):
+        # Display the words with their indeces.
+        for tup in zip(tokens, index_tokens):
+            print('{:<12} {:>6,}'.format(tup[0], tup[1]))
+
+
+    def addPadding(self, T: int, tokens: torch.Tensor) -> torch.Tensor:
+        padded_tokens = tokens + ['[PAD]' for _ in range(T-len(tokens))]
+        print("Padded tokens are \n {} ".format(padded_tokens))
+        attn_mask = [ 1 if token != '[PAD]' else 0 for token in padded_tokens  ]
+        print("Attention Mask are \n {} ".format(attn_mask))
 
     def embed(self, tokens: torch.Tensor) -> torch.Tensor:
-        print("embeddings", self.model.embeddings.word_embeddings(tokens))
-
-        for e in self.model.embeddings.word_embeddings(tokens)[0]:
-            print("This is an embedding!", e[0])
+        from sentence_transformers import SentenceTransformer
+        model = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
+        embeddings = model.encode(tokens)
+        return embeddings
+        # return self.model.embeddings.word_embeddings(tokens)
 
     def model_forward(self, tokens: torch.Tensor) -> torch.Tensor:
         output = self.model(tokens)
--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-from llmex.llm import LLM
+from llmex.llm import LLM, cosine_similarity
 
 text = "How do I unscrew a screw without a screwdriver?"
 document = """Kitchen butter knives can be used in a very similar way to coins. Insert the end of the butter knife into the longer groove and turn counterclockwise to unscrew the screw.
 If your butter knife is of low quality and strength or the screw is very tight, then you may bend your butter knife rather than unscrewing the screw. Be aware of this potential damage.[1]"""
 
 
-model_name = "bert-base-cased"
-lm = LLM(name="llm", model_name=model_name, tokenizer_name=model_name)
+model_checkpoint = "bert-base-cased"
+lm = LLM(name="llm", model_name=model_checkpoint, tokenizer_name=model_checkpoint)
 
-tokens = lm.tokenize()
-embeddings = lm.embed(tokens)
+tokens_text = lm.tokenizer.tokenize(text)
+indexed_tokens = lm.tokenizer.convert_tokens_to_ids(tokens_text)
 
-lm.describe_model()
+lm.display_tokenized_text(tokens_text, indexed_tokens)
 
-# print(lm.model_forward(tokens))
+tokens_document = lm.tokenizer.tokenize(document)
+index_tokend_document = lm.tokenizer.convert_tokens_to_ids(tokens_document)
+
+lm.display_tokenized_text(tokens_document, index_tokend_document)
+
+# embeddings_text = lm.embed(text)
+# embeddings_document = lm.embed(document)
+
+# # max_tokens = max(len(tokens_text), len(tokens_document))
+
+# # vector_a = embeddings_text[0][0]
+# # vector_b = embeddings_document[0][0]
+# # len(vector_a)
+# # len(vector_b)
+
+# similarity_score = cosine_similarity(embeddings_text, embeddings_document)
+# # similarity_score = cosine_similarity(vector_a, vector_b)
+# print("similarity_score: ", similarity_score)
