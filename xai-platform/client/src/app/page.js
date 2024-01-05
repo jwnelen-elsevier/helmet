1e9a73b3ce464896824949f752cb7532d0ba2965
jwnelen-elsevier <jnelen@elsevier.com>
2024-01-05 20:42:24 +0100

    footer added

--- xai-platform/client/src/app/page.js
+++ xai-platform/client/src/app/page.js
@@ -1,7 +1,29 @@
+import ModelCard from "@/components/ModelCard";
+
 export const metadata = {
-  title: "My Page Title",
+  title: "XAI Platform",
 };
 
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+const serverUrl = process.env.SERVER_URL ?? "http://localhost:4000";
+
+async function fetchExplainerData() {
+  const res = await fetch(`${serverUrl}/explainer`);
+  if (!res?.ok) {
+    throw new Error("Error fetching explainer");
+  }
+
+  return res.json();
 }
+
+export default async function Page() {
+  const data = await fetchExplainerData();
+
+  console.log(data);
+  return (
+    <div>
+      <ModelCard model={data.model}></ModelCard>
+    </div>
+  );
+}
+
+// TODO: Add revalidatePath when we delete all state.
---
+++ xai-platform/client/src/components/footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="absolute bottom-0 left-0 w-full outline outline-gray-200">
+      <div className="w-full mx-auto text-center p-1">XAI platform </div>
+    </footer>
+  );
+};
+
+export default Footer;
