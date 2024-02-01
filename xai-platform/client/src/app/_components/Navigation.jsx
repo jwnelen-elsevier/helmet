00740f84b046128cdedfde24311bfb458a3137a3
jwnelen-elsevier <jnelen@elsevier.com>
2024-02-01 18:12:54 +0100

    Small changes

--- xai-platform/client/src/app/_components/Navigation.jsx
+++ xai-platform/client/src/app/_components/Navigation.jsx
@@ -14,7 +14,7 @@ const NagivationBar = () => {
 
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <div className="flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
             LLM XAI Platform
--- xai-platform/client/src/app/page.js
+++ xai-platform/client/src/app/page.js
@@ -3,7 +3,6 @@ import ModelCard from "./_components/ModelCard";
 export default async function Page() {
   return (
     <div className="w-full h-full text-center m-auto">
-      Could not load data Model name: <br></br>
       <ModelCard></ModelCard>
     </div>
   );
