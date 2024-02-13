bbd93e1b2b1eb256cb2a4f0ba71ecaa6fbe5d842
jwnelen-elsevier <jnelen@elsevier.com>
2024-02-13 08:36:05 +0100

    Copy button is working

--- webapp/client/src/app/_components/detailDisplayer.jsx
+++ webapp/client/src/app/_components/detailDisplayer.jsx
@@ -4,26 +4,57 @@ import Link from "next/link";
 
 import TextHighlighter from "@/app/_components/textHighlighter";
 import { QuestionIcon } from "@/app/_components/ui/icons";
+import { CopyIcon, CopySucceedIcon } from "./ui/icons";
+import clsx from "clsx";
 
 const DetailDisplayer = ({ props }) => {
   const { date, output, input, explanation, input_tokens, _id } = props;
   const { input_attribution, explanation_method } = explanation;
   const [showAttributions, s] = useState(false);
 
+  const [isCopied, setCopied] = useState(false);
+
+  const copyToClipboard = (text) => {
+    navigator.clipboard.writeText(text);
+    setCopied(true);
+  };
+
   return (
-    <div className="flex flex-row py-2 ">
-      <TextHighlighter
-        tokens={input_tokens}
-        attributions={input_attribution}
-        showAttributions={showAttributions}
-      />
-      <p className="px-2">Classified: {output}</p>
-      <p>
-        {`Attribution method: ${explanation_method}`}
-        <Link href={"/resources#gradientxinput"}>
-          <QuestionIcon />
-        </Link>
-      </p>
+    <div className="flex flex-col items-center">
+      <div className="flex flex-row py-2 ">
+        <TextHighlighter
+          tokens={input_tokens}
+          attributions={input_attribution}
+          showAttributions={showAttributions}
+        />
+        <p className="px-2">Classified: {output}</p>
+        <p>
+          {`Attribution method: ${explanation_method}`}
+          <Link href={"/resources#gradientxinput"}>
+            <QuestionIcon />
+          </Link>
+        </p>
+      </div>
+      <div className="flex flex-row gap-2">
+        <button
+          onClick={() => s(!showAttributions)}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Show attributions
+        </button>
+      </div>
+      <div className="flex flex-row items-center group">
+        {_id}
+        <button
+          onClick={() => copyToClipboard(_id)}
+          className={clsx(
+            " cursor-pointer text-black",
+            isCopied ? "opacity-100" : "opacity-0 group-hover:opacity-100"
+          )}
+        >
+          {isCopied ? <CopySucceedIcon /> : <CopyIcon />}
+        </button>
+      </div>
     </div>
   );
 };
--- webapp/client/src/app/_components/ui/icons/index.js
+++ webapp/client/src/app/_components/ui/icons/index.js
@@ -4,5 +4,16 @@ import QuestionIcon from "./question";
 import MenuIcon from "./menu";
 import BackIcon from "./back";
 import NextIcon from "./next";
+import CopyIcon from "./copy";
+import CopySucceedIcon from "./copySucceed";
 
-export { DeleteIcon, DetailsIcon, QuestionIcon, MenuIcon, BackIcon, NextIcon };
+export {
+  DeleteIcon,
+  DetailsIcon,
+  QuestionIcon,
+  MenuIcon,
+  BackIcon,
+  NextIcon,
+  CopyIcon,
+  CopySucceedIcon,
+};
