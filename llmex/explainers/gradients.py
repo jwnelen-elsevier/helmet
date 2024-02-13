7eb4bda8ac3d82dcdb7408d7c834e51e3fca68b8
jwnelen-elsevier <jnelen@elsevier.com>
2024-02-13 14:54:42 +0100

    Extracted the gradient calculation from the baseLLM

--- llmex/explainers/gradients.py
+++ llmex/explainers/gradients.py
@@ -1,7 +1,28 @@
-from ferret.explainers.gradient import GradientExplainer
+from captum.attr import InputXGradient, Saliency
+from typing import Dict, Any
+from functools import partial
+from llmex.baseLM import BaseLM
+from torch.nn import functional as F
 
-def computeGradients(model, tokenizer, prompt, class_id):
-    gradient_explainer = GradientExplainer(model, tokenizer)
-    res = gradient_explainer.compute_feature_importance(prompt, class_id)
-    return res
+def compute_gradient(wrapper: BaseLM, prompt, input, output, gradient_type):
+    def model_forward(inp, model, extra_forward_args: Dict[str, Any] = {}):
+            output = model(inputs_embeds=inp, **extra_forward_args)
+            return F.softmax(output.logits, dim=1)
+    
+    input_embeds = wrapper.get_input_embeddings(prompt)
+    attention_mask = input["attention_mask"]
+
+    forward_func = partial(model_forward, model=wrapper.model, extra_forward_args={"attention_mask": attention_mask})
+
+    if gradient_type == "input_x_gradient":
+        lig = InputXGradient(forward_func)
+    else:
+        lig = Saliency(forward_func)
+    attributions = lig.attribute(inputs=input_embeds, target=output)
+    attributions = attributions.detach().cpu().numpy()
+    r = attributions[0, :, :]
+    attr = r.sum(axis=1)
+    attr = wrapper.normalize(attr)
+
+    return attr
 
