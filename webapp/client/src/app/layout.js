3a4855d748e3db5abc210629369464ad12c97b01
jwnelen-elsevier <jnelen@elsevier.com>
2024-02-16 13:51:44 +0100

    Setup of NextUI

--- webapp/client/src/app/layout.js
+++ webapp/client/src/app/layout.js
@@ -3,6 +3,7 @@ import FooterComponent from "@/app/_components/footer";
 import "@/styles/globals.css";
 import { StatusProvider } from "@/providers/status";
 import PageNav from "@/app/_components/pageNav";
+import { Providers } from "@/providers/nextUI";
 
 export default function RootLayout({ children }) {
   return (
--- webapp/client/tailwind.config.js
+++ webapp/client/tailwind.config.js
@@ -1,19 +1,17 @@
 /**
  * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
  */
+import { nextui } from "@nextui-org/react";
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/flowbite-react/lib/**/*.js",
-  ],
-  safelist: [
-    {
-      pattern: /bg-/,
-    },
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
+  plugins: [nextui()],
 };
