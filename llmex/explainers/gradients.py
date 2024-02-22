bee0e57d16f6352569efd31e36745a0a33ca79f0
jwnelen-elsevier <jnelen@elsevier.com>
2024-02-22 09:17:38 +0100

    Refactor compute_gradients_causal function in gradients.py

--- llmex/explainers/gradients.py
+++ llmex/explainers/gradients.py
@@ -30,17 +30,6 @@ def compute_gradient(wrapper, prompt, input, output, gradient_type):
 
 # For decoder models, only Layered Integrated Gradients is supported
 def compute_gradients_causal(wrapper, prompt, output):
-    def model_forward(inp, model, extra_forward_args: Dict[str, Any] = {}):
-        max_new_tokens = 5
-        return model.generate(
-            input_ids=inp["input_ids"],
-            use_cache=True, 
-            max_new_tokens=max_new_tokens,
-            **extra_forward_args
-        )
-    
-    # forward_func = partial(model_forward, model=wrapper.model, extra_forward_args={})
-
     # LayerIntegratedGradients is only supported for decoder models
     ig = LayerIntegratedGradients(wrapper.model, wrapper._get_embedding_layer())
 
@@ -54,5 +43,5 @@ def compute_gradients_causal(wrapper, prompt, output):
         skip_tokens=[1],  # skip the special token for the start of the text <s>
     )
     
-    res = llm_attr.attribute(input)
+    res = llm_attr.attribute(input, target=output)
     return res
