d609603aaba4c2ed07c21c98f094959c6b7ea068
jwnelen-elsevier <jnelen@elsevier.com>
2024-05-07 08:53:50 +0200

    Updated API limit

--- webapp/api/app.js
+++ webapp/api/app.js
@@ -16,7 +16,11 @@ var app = express();
 
 //config
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(
+  express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
+);
+// app.use(bodyParser.json());
 
 const State = require("./state");
 const s = new State();
